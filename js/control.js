c3445331bfa7deac5287d29c495977a8fc3592c0
https://github.com/hai56dung/C4E_Homeword.git <haidung0902@gmail.com>
2021-06-23 00:41:20 +0700

    add Login

---
+++ js/control.js
@@ -0,0 +1,23 @@
+let userEl = document.querySelector(".txtuser");
+let passEl = document.querySelector(".txtpass");
+let submit = document.querySelector(".submit");
+
+if(window.localStorage){
+    localStorage.setItem("user","hai56dung");
+    localStorage.setItem("pass","nguyenhaidung123");
+
+    let user = localStorage.getItem("user");
+    let pass = localStorage.getItem("pass");
+
+    let message = document.querySelector(".message");
+
+    submit.addEventListener("click", function() {
+        if(user == userEl.value && pass == passEl.value){
+            window.location = "home.html"
+        }else{
+            message.innerHTML = `Đăng nhập không thành công`;
+        }
+    })
+}else{
+    console.log("not Supported..");
+}
